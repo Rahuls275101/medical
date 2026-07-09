2c8ce27514e7b723d46861f3fcd42f485a32f07d
rahulsharma275101 <r.sharma275101@gmail.com>
2026-07-09 10:50:15 +0530

    change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Configurations for different paths
-const baseUrl = 'https://snindustries.org/predy'; 
+const baseUrl = 'http://localhost:3000'; 
 const apiBaseUrl = 'https://api.genxmedicaldevices.com/api/admin'; // Replace with your API base URL
 const apiUrl = 'https://api.genxmedicaldevices.com'; 
 export { baseUrl,apiBaseUrl,apiUrl };
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename="/predy">
+      <BrowserRouter basename="">
         <App />
       </BrowserRouter>
     </HelmetProvider>
